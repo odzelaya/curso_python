56e7a50b7e67cb193c3ebe57b2f7cf3e862ee799
Oscar Zelaya <odmedina89@gmail.com>
2025-04-08 14:46:12 -0600

    Explicando tipos y variables en python

--- tipos_variables.py
+++ tipos_variables.py
@@ -1,4 +1,5 @@
 # Curso introductorio a python
+import string
 
 # ** Tipo de datos primitivos y creación de variables **
 # Pytohn define sus datos primitivos como Textos, Numeros y Booleanos
@@ -19,9 +20,62 @@ numero_flotante = 0.05
 booleano = False
 numero_complejo = complex(2,5)
 
-print(type(nombre))
-print(type(numero_entero))
-print(type(numero_flotante))
-print(booleano, type(booleano))
-print(numero_complejo, type(numero_complejo))
+
+# print(type(nombre))
+# print(type(numero_entero))
+# print(type(numero_flotante))
+# print(booleano, type(booleano))
+# print(numero_complejo, type(numero_complejo))
+
+
+# ** Manejo de cadenas de textos **
+
+# Las cadenas de textos (str) en python se comportan con una lista (estructura de datos) a las cuales se les pueden
+# aplicar metodos propios de las listas.
+
+#** Constantes de la clase string **#
+
+# print(type(string.ascii_letters))
+# print(string.ascii_uppercase)
+# print(string.ascii_lowercase)
+# print(type(string.punctuation))
+# print(string.digits)
+# print(string.whitespace.split())
+
+#** Manipulando cadenas de textos **#
+
+nombre = "Vicente Fernandez"
+apellido = "Gómez"
+
+nombre_completo = nombre + ' ' + apellido
+
+# Imprimir nombre completo
+print(nombre_completo)
+
+# Accediendo a un valor (caracter) de la de la cadena
+print(nombre_completo[0])
+print(nombre_completo[1])
+print(nombre_completo[2])
+print(nombre_completo[-1])
+print(nombre_completo[-2])
+
+# Accediendo a porciones del texto
+print(nombre_completo[0:4])
+print(nombre_completo[:])
+print(nombre_completo[3:6])
+print(nombre_completo[:5])
+print(nombre_completo[:-2])
+
+
+mensaje = 'Bienvenido {} al curso de Python!'.format('Joel')
+mensaje2 = f'Bienvenido {nombre_completo} al curso de Python!'
+
+
+
+print(mensaje2.upper())
+print(mensaje2.lower())
+print('1234'.isnumeric())
+
+
+
 
